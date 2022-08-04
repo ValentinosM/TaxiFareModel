27068ce5fd02b4c19da1b3a695754191f92ba135
Valentinos Mouxouris <v.mouxouris@hotmail.com>
2022-08-04 21:49:04 +0100

    working on Trainer.py and adding more function

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -7,6 +7,15 @@ from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from TaxiFareModel.utils import compute_rmse
+from memoized_property import memoized_property
+from mlflow.tracking import MlflowClient
+import mlflow
+import joblib
+from sklearn.linear_model import SGDRegressor
+from sklearn.ensemble import AdaBoostRegressor
+
+EXPERIMENT_NAME = "[GB] [London] [ValentinosM] TaxiFareModel version 1.0"
+MLFLOW_URI = "https://mlflow.lewagon.ai/"
 
 
 class Trainer():
@@ -18,6 +27,8 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = EXPERIMENT_NAME
+
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -31,18 +42,47 @@ class Trainer():
         self.pipeline = pipe
 
 
-
     def run(self):
         """set and train the pipeline"""
         self.set_pipeline()
-        self.pipeline.fit(self.X,self.y)
+        self.pipeline.fit(self.X_train,self.y_train)
+        self.mlflow_log_param('model','Linear')
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric('rmse',rmse)
         print(rmse)
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline,'model.joblib')
+
 
 
 if __name__ == "__main__":
@@ -54,3 +94,8 @@ if __name__ == "__main__":
     trainer = Trainer(X_train,y_train)
     trainer.run()
     trainer.evaluate(X_val,y_val)
+    experiment_id = trainer.mlflow_experiment_id
+    print(f"experiment URL: https://mlflow.lewagon.ai/#/experiments/{experiment_id}")
+    my_model = trainer.save_model()
+    # models = [SGDRegressor(),AdaBoostRegressor()]
+    # for model in models:
